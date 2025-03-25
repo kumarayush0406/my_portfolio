7a1525168ade463eb31e51067a6c044b35ca886d
kumarayush0406 <112921096+kumarayush0406@users.noreply.github.com>
2025-03-25 07:49:49 +0530

    Update App.jsx (HeroMain path)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import NavbarMain from "./components/navbar/NavbarMain";
-import HeroMain from "./components/heroSection/heroMain";
+import HeroMain from "./components/heroSection/HeroMain";
 import HeroGradient from "./components/heroSection/heroGradient";
 import SubHeroSection from "./components/heroSection/SubHeroSection";
 import AboutMeMain from "./components/AboutMeSection/AboutMeMain";
