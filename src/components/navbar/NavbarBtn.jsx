bf7e1454ce18e60998615e171116a1b39a4cb315
kumarayush0406 <112921096+kumarayush0406@users.noreply.github.com>
2025-03-25 08:56:25 +0530

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import { RiArrowRightDownLine } from "react-icons/ri";
 const NavbarBtn = () => {
   return (
     <button className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-centre gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow ">
-     <a href="https://drive.google.com/file/d/10aJLoStAgGwX_iX9BU_x3wYszfiY3FDe/view?usp=drivesdk"> My Resume</a>
+     <a href="https://drive.google.com/file/d/10aJLoStAgGwX_iX9BU_x3wYszfiY3FDe/view?usp=drivesdk">Resume</a>
       <div className="sm:hidden md:block">
       <RiArrowRightDownLine /></div>
     </button>
