b30a6b8efcb1bc14df4149c5e219c7e42b6fe28f
kumarayush0406 <112921096+kumarayush0406@users.noreply.github.com>
2025-03-25 09:06:32 +0530

    Update SubSkills.jsx

--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -5,7 +5,7 @@ const SubSkills = () => {
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full">
         {" "}
       </div>
-      <img src="../../public/images/subSkills.jpg" alt="subskills" />
+      <img src="../../images/subSkills.jpg" alt="subskills" />
     </div>
   );
 };
